99e49062d860b3987b8dfcac1b4bced9976a16e7
rokujohaven <43080066+rokujohaven@users.noreply.github.com>
2018-11-18 23:44:55 -0500

    Create longestWordinStr.js

---
+++ longestWordinStr.js
@@ -0,0 +1,15 @@
+//https://stackoverflow.com/questions/1044105/how-do-you-get-the-length-of-a-string
+
+function findLongestWordLength(str) {
+ let words = str.split(" ");
+ let longestWord = 0;
+ for (let i = 0; i < words.length; ++i){
+   if (words[i].length > longestWord){
+     longestWord = words[i].length;
+   }
+ }
+ return longestWord;
+}
+
+//test
+findLongestWordLength("The quick brown fox jumped over the lazy dog");

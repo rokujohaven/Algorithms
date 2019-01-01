77829db8df56ba255b908c9ba08483ab82464b19
rokujohaven <43080066+rokujohaven@users.noreply.github.com>
2019-01-01 13:49:07 -0500

    Update frankenSplice.js

--- frankenSplice.js
+++ frankenSplice.js
@@ -5,10 +5,24 @@
 
 function frankenSplice(arr1, arr2, n) {
   // It's alive. It's alive!
-  let arr=[...arr2]; //to avoid mutating the input arrays
+  let arr=[...arr2]; //to avoid mutating the input arrays ES6
   arr.splice(n,0,arr1);
   return arr;
 }
 
 console.log(frankenSplice([1, 2, 3], [4, 5], 1));
 4,1,2,3,5
+
+//alternative implementation
+function frankenSplice(arr1, arr2, n) {
+  // It's alive. It's alive!
+  let arr = arr2.slice(); 
+  for (let i = 0; i < arr1.length; ++i){
+     arr.splice(n,0,arr1[i]);
+     n++;
+  }
+  return arr;
+}
+
+console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
+

a5ce27d2819ec38f6c301dab4c198748e438671d
rokujohaven <43080066+rokujohaven@users.noreply.github.com>
2019-01-16 11:40:00 -0500

    Update FinChallenge1_PalindromeChecker.js

--- FinChallenge1_PalindromeChecker.js
+++ FinChallenge1_PalindromeChecker.js
@@ -4,6 +4,7 @@
 
 //number values are checked, but special characters and spaces are ignored. 
 
+//Method 1: Brute force
 function palindrome(str) {
   //clean format
   let pattern = /[A-Za-z0-9]/ig;
@@ -22,8 +23,43 @@ function palindrome(str) {
   return false; 
 }
 
+//Method 2: Front and back comparison
+function palindrome(str) {
+  // This solution is most efficient since it does not need to check the whole string even once. Good for long strings. 
+  //it just finds 2 letters that don't match and returns not a palindrome
 
+  //make front and back indexes 
+  let front = 0; 
+  let back = str.length - 1; 
 
+  //catch all non word characters and ignores them from the front and the back. 
+  while(back > front){
+
+    //ignoring special characters and spacing from front and back. 
+    if (str[front].match(/[\W_]/)){
+    front++;
+    continue; 
+    }
+    if(str[back].match(/[\W_]/)){
+    back--;
+    continue; 
+    }
+    
+    //at this point, front and back should both index an alphanumeric char
+    //convert to lower case and compare
+    if(str[front].toLowerCase() !== str[back].toLowerCase()){
+      return false; 
+    }
+    //if both front and back alphanumeric chars match, proceed to next 
+    front++;
+    back--;
+
+  }
+  
+  return true;
+}
+
+console.log(palindrome("almostomla")); //false
 console.log(palindrome("2_A3*3#A2")); //true 
 console.log(palindrome("A man, a plan, a canal. Panama")); //true
 console.log(palindrome("five|\_/|four")); //false 

2e911b811485056c196f0266d67a73f26154bff6
rokujohaven <43080066+rokujohaven@users.noreply.github.com>
2019-03-06 03:58:41 -0500

    Update spinalCase.js

--- spinalCase.js
+++ spinalCase.js
@@ -1,3 +1,5 @@
+//Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+
 function spinalCase(str) {
   // "It's such a fine line between stupid, and clever."
   // --David St. Hubbins

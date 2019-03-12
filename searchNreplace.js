a703047b4b2f6361d4f9d5d756a879ea53d51c20
rokujohaven <43080066+rokujohaven@users.noreply.github.com>
2019-03-12 00:55:25 -0400

    Update searchNreplace.js

--- searchNreplace.js
+++ searchNreplace.js
@@ -4,7 +4,7 @@
 //Third argument is what you will be replacing the second argument with (after).
 //Note: Preserve the case of the first character in the original word when you are replacing it. For example if you mean to replace the word "Book" with the word "dog", it should be replaced as "Dog"
 
-//using basic loops
+//using basic loops (no case sensitivity)
 function myReplaceLoop(str, before, after) {
   let arr = str.split(/\s/); 
   console.log(arr);
@@ -17,12 +17,29 @@ function myReplaceLoop(str, before, after) {
   return str; 
 }
 
-//using map
+//using map (with case sensitive to first char in before)
 function myReplace(str, before, after) {
   let arr = str.split(/\s/); 
-  arr = arr.map(x => {if(x == before){x = after} return x})
+  arr = arr.map(x => {if(x == before){
+                        if(x[0] == x[0].toUpperCase()){
+                          x = after;
+                          x = x[0].toUpperCase() + x.substring(1);
+                    
+                        } 
+                        else{
+                        x = after;     
+                                         
+                        }
+                                }////// 
+                        else{//do nothing
+                        }
+                        return x;
+                        });
   str = arr.join(" ");
   console.log(str);
   return str; 
 }
+
 myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+myReplace("He is Sleeping on the couch", "Sleeping", "sitting");
+myReplace("His name is Tom", "Tom", "john");

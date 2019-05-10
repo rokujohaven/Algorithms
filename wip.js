53013a677094569131550e803addb0c1879ff801
rokujohaven <43080066+rokujohaven@users.noreply.github.com>
2019-05-10 17:48:55 -0400

    Update wip.js

--- wip.js
+++ wip.js
@@ -2,27 +2,35 @@ function checkCashRegister(price, cash, cid) {
 // Here is your change, ma'am.
   var change = cash - price;
   let reducer = (accumulator, currentvalue) => accumulator + currentvalue[1]; 
+  let moneyAvail = cid.reduce(reducer,0); 
+  let unit_Val = {"PENNY":.01, "NICKEL":.05, "DIME":.1, "QUARTER":0.25, "ONE":1, "FIVE":5, "TEN":10, "TWENTY":20, "ONE HUNDRED":100};
+
   if(change < 0){
-    return "ERROR: CLIENT DOES NOT HAVE ENOUGH MONEY TO BUY ITEM"
+    return "ERROR: CLIENT DOES NOT HAVE ENOUGH MONEY TO BUY ITEM";
   }
   else if (change == 0){ //if exact change
     return {status: "CLOSED", change:cid};
   }
-  else if(cid.reduce(reducer, 0) < change){ //if not enough money in drawer
+  else if(moneyAvail < change){ //if not enough money in drawer
     return {status: "INSUFFICIENT_FUNDS", change: []};
   }
   else{ //if change is needed and drawer has enough money
     let due = []; 
     for (let i = cid.length-1; i > -1; --i)
     {
-     if(cid[i][1] < change){
-       continue;
-     }
-     else{
-       change = 
-     }
+      //if change is less than a particular currency unit, we cannot use that unit
+        if(change < unit_Val[cid[i][0]]){
+            console.log("passing over: " + cid[i][0]);
+            continue; 
+        }
+        else{
+            console.log("testing " + cid[i][0] + "...$" + cid[i][1]); 
+            if (cid[i][1]>change){
+              
+            }
+        }
     }
-    return{status: "OPEN", change:[]};
+    return{status: "OPEN", change:due};
   }
   
 }
